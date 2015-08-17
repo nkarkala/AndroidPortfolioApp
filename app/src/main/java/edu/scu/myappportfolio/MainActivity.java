c018e6e50be0a8f3033781fc4a1046a4ee465829
Nikitha <nikithashenoyk@gmail.com>
2015-08-16 23:27:49 -0700

    Main activity with links to apps

---
+++ app/src/main/java/edu/scu/myappportfolio/MainActivity.java
@@ -0,0 +1,46 @@
+package edu.scu.myappportfolio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void respondToButtonClick(View view) {
+        Toast.makeText(this,"This button will launch " + view.getContentDescription(),Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
